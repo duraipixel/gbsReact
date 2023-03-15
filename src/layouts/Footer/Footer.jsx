db1c8daaf646aa04219478526ab26a546fb4ee18
Fahd <fahd.pixel@gmail.com>
2023-03-15 15:32:18 +0530

    FEAT:Footer

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+const Footer = () => {
+  return <div></div>;
+};
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../layouts/Header/Header";
 import HomeProductsSlider from "components/Home/HomeProductsSlider/HomeProductsSlider";
-import PackageSupport from "components/Home/PackageSupport/PackageSupport"
+import PackageSupport from "components/Home/PackageSupport/PackageSupport";
+import Footer from "layouts/Footer/Footer";
 const HomePage = () => {
   return (
     <div>
       <Header />
       <HomeProductsSlider />
       <PackageSupport />
+      <Footer />
     </div>
   );
 };
