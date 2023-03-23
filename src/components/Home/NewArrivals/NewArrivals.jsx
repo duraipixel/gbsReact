85813dbea2ad8b1fddc7923d7b03794117cb3277
behin-pixel <behin.pixel@gmail.com>
2023-03-23 16:43:00 +0530

    Middle

--- src/components/Home/NewArrivals/NewArrivals.jsx
+++ src/components/Home/NewArrivals/NewArrivals.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Slider from "react-slick";
-import { FiChevronRight } from "react-icons/fi";
-import Poster1 from "assets/images/HomeProductsPoster/Poster1.png";
+import { FiChevronRight, FiHeart } from "react-icons/fi";
+import { AiFillStar } from "react-icons/ai";
+import { BiGitCompare } from "react-icons/bi";
+import Poster1 from "assets/images/arrivals/product-1.jpg";
+import Poster2 from "assets/images/arrivals/product-2.jpg";
+import Poster3 from "assets/images/arrivals/product-3.jpg";
+import Poster4 from "assets/images/arrivals/product-4.jpg";
+import Poster5 from "assets/images/arrivals/product-5.jpg";
 import "./styles.css";
 
 const NewArrivals = () => {
@@ -11,7 +17,7 @@ const NewArrivals = () => {
     arrows: false,
     swipe: true,
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -30,11 +36,175 @@ const NewArrivals = () => {
     </div>
     </div> 
 
+    <div className="arrival-slider">
     <Slider {...settings}>
-      <div>
-         
-      </div>
+    
+    <div className="arival-det">
+    <div className="ari-img">
+    <img src={Poster1} alt="" className="img-fluid poster-image" />
+    <div className="off-prc">
+    <h3> 30% <span>OFF</span></h3>
+    </div>
+    </div>
+    
+    <div className="ari-cnt text-center">
+    <div className="d-flex justify-content-between">
+    <h2>Laptops</h2>
+    <h3><AiFillStar /> 4.5</h3>
+    </div>
+    <h4>Dell Vostro 3420 -Carbon Black</h4>
+    <h5><span>₹83,127</span> ₹57,990</h5>
+    <ul>
+    <li>12th Gen Intel Core i5-1235U</li>
+    <li>8GB, DDR4, 2666 MHz</li>
+    <li>512GB SSD Storage</li>
+    <li>Intel Integrated Graphics</li>
+    </ul>
+    <div className="d-flex justify-content-between">
+    <div className="clk-optn">
+    <a href=""><FiHeart /> </a>
+    <a href=""><BiGitCompare /></a>
+    </div>
+    <div>
+    <a className="red-bg-btn" href="">Add to cart</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    <div className="arival-det">
+    <div className="ari-img">
+    <img src={Poster2} alt="" className="img-fluid poster-image" />
+    <div className="off-prc">
+    <h3> 30% <span>OFF</span></h3>
+    </div>
+    </div>
+    
+    <div className="ari-cnt text-center">
+    <div className="d-flex justify-content-between">
+    <h2>Laptops</h2>
+    <h3><AiFillStar /> 4.5</h3>
+    </div>
+    <h4>Dell Vostro 3420 -Carbon Black</h4>
+    <h5><span>₹83,127</span> ₹57,990</h5>
+    <ul>
+    <li>12th Gen Intel Core i5-1235U</li>
+    <li>8GB, DDR4, 2666 MHz</li>
+    <li>512GB SSD Storage</li>
+    <li>Intel Integrated Graphics</li>
+    </ul>
+    <div className="d-flex justify-content-between">
+    <div className="clk-optn">
+    <a href=""><FiHeart /> </a>
+    <a href=""><BiGitCompare /></a>
+    </div>
+    <div>
+    <a className="red-bg-btn" href="">Add to cart</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    <div className="arival-det">
+    <div className="ari-img">
+    <img src={Poster3} alt="" className="img-fluid poster-image" />
+     
+    </div>
+    
+    <div className="ari-cnt text-center">
+    <div className="d-flex justify-content-between">
+    <h2>Laptops</h2>
+    <h3><AiFillStar /> 4.5</h3>
+    </div>
+    <h4>Dell Vostro 3420 -Carbon Black</h4>
+    <h5><span>₹83,127</span> ₹57,990</h5>
+    <ul>
+    <li>12th Gen Intel Core i5-1235U</li>
+    <li>8GB, DDR4, 2666 MHz</li>
+    <li>512GB SSD Storage</li>
+    <li>Intel Integrated Graphics</li>
+    </ul>
+    <div className="d-flex justify-content-between">
+    <div className="clk-optn">
+    <a href=""><FiHeart /> </a>
+    <a href=""><BiGitCompare /></a>
+    </div>
+    <div>
+    <a className="red-bg-btn" href="">Add to cart</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    <div className="arival-det">
+    <div className="ari-img">
+    <img src={Poster4} alt="" className="img-fluid poster-image" />
+    <div className="off-prc">
+    <h3> 30% <span>OFF</span></h3>
+    </div>
+    </div>
+    
+    <div className="ari-cnt text-center">
+    <div className="d-flex justify-content-between">
+    <h2>Laptops</h2>
+    <h3><AiFillStar /> 4.5</h3>
+    </div>
+    <h4>Dell Vostro 3420 -Carbon Black</h4>
+    <h5><span>₹83,127</span> ₹57,990</h5>
+    <ul>
+    <li>12th Gen Intel Core i5-1235U</li>
+    <li>8GB, DDR4, 2666 MHz</li>
+    <li>512GB SSD Storage</li>
+    <li>Intel Integrated Graphics</li>
+    </ul>
+    <div className="d-flex justify-content-between">
+    <div className="clk-optn">
+    <a href=""><FiHeart /> </a>
+    <a href=""><BiGitCompare /></a>
+    </div>
+    <div>
+    <a className="red-bg-btn" href="">Add to cart</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    <div className="arival-det">
+    <div className="ari-img">
+    <img src={Poster5} alt="" className="img-fluid poster-image" />
+    <div className="off-prc">
+    <h3> 30% <span>OFF</span></h3>
+    </div>
+    </div>
+    
+    <div className="ari-cnt text-center">
+    <div className="d-flex justify-content-between">
+    <h2>Laptops</h2>
+    <h3><AiFillStar /> 4.5</h3>
+    </div>
+    <h4>Dell Vostro 3420 -Carbon Black</h4>
+    <h5><span>₹83,127</span> ₹57,990</h5>
+    <ul>
+    <li>12th Gen Intel Core i5-1235U</li>
+    <li>8GB, DDR4, 2666 MHz</li>
+    <li>512GB SSD Storage</li>
+    <li>Intel Integrated Graphics</li>
+    </ul>
+    <div className="d-flex justify-content-between">
+    <div className="clk-optn">
+    <a href=""><FiHeart /> </a>
+    <a href=""><BiGitCompare /></a>
+    </div>
+    <div>
+    <a className="red-bg-btn" href="">Add to cart</a>
+    </div>
+    </div>
+    </div>
+    </div>
+
     </Slider>
+    </div>
+
 
     </div>
     </div>
--- src/components/Home/ServiceCenter/ServiceCenter.jsx
+++ src/components/Home/ServiceCenter/ServiceCenter.jsx
@@ -1,12 +1,41 @@
 import React from "react";
-import "./styles.css";
-import bg from "assets/images/ServiceCenter/Rectangle149.png";
+import "./styles.css"; 
 
 const ServiceCenter = () => {
   return (
-    <div>
-      <img src={bg} alt="" className="img-fluid" />
+    <>
+
+    <section className="ran-trouble text-center">
+    <div className="container">
+    <div className="row">
+
+    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div className="comon-heads">
+    <h2>Your PC Ran into Trouble?</h2>
     </div>
+    </div>
+
+    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div className="details-locations">
+    <span>Don't worry, the experts at GBS Services can help you fix issues <br />
+    with your PC in no time</span>
+    <h4>23 Service Centers Across South India</h4>
+    <ul>
+    <li>Chennai</li>
+    <li>Madurai</li>
+    <li>Trichy</li>
+    <li>Coimbatore</li>
+    <li>Bangalore</li>
+    </ul>
+    <a href="">Find Your Nearest GBS After Sales Service Center</a>
+    </div>
+    </div>
+
+    </div>
+    </div>
+    </section>
+      
+    </>
   );
 };
 
