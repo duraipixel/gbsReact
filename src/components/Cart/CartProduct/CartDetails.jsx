bdbafc9bd53e756cd4d7d1910547d4d3bac0a386
Fahd <fahd.pixel@gmail.com>
2023-03-27 16:45:56 +0530

    FEAT: Cart Details Component in Cart Page

--- src/components/Cart/CartProduct/CartDetails.jsx
+++ src/components/Cart/CartProduct/CartDetails.jsx
@@ -1,45 +1,102 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import razorpay from "assets/images/Cart/razorpay.png";
 const CartDetails = () => {
   return (
-    <div>
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
       <h4>Cart Details</h4>
       <h5 className="clr-accent">Cart Subtotal</h5>
       <table className="table table-borderless">
         <tbody>
           <tr>
-            <td>Sub Total</td>
-            <td>₹98,506</td>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>Sub Total</td>
+            <td style={{ paddingRight: "0", textAlign: "right" }}>₹98,506</td>
           </tr>
           <tr>
-            <td>Taxes</td>
-            <td>₹17,731.08</td>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>Taxes</td>
+            <td style={{ paddingRight: "0", textAlign: "right" }}>
+              ₹17,731.08
+            </td>
           </tr>
         </tbody>
       </table>
       <div className="line-spacer"></div>
-
+      <h5 className="clr-accent">Select Shipping Speed</h5>
       <table className="table table-borderless">
         <thead>
-          <h5 className="clr-accent">Select Shipping Speed</h5>
+          <tr>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}> Ship To:</td>
+            <td
+              style={{
+                position: "absolute",
+                right: "0",
+                paddingRight: "0",
+              }}
+            >
+              <Link to="/" className="change-address">
+                Change Address
+              </Link>
+            </td>
+          </tr>
         </thead>
         <tbody>
-          <tr>
-            <td> Ship To:</td>
-            <td> Change Address</td>
+          <tr colSpan="2">
+            Kabir L <br />
+            1833, 18th Main Road, Thiruvalluvar Colony, Anna Nagar West,
+            Chennai, Tamil Nadu 600040
           </tr>
           <tr>
-            Kabir L 1833, 18th Main Road, Thiruvalluvar Colony, Anna Nagar West,
-            Chennai, Tamil Nadu 600040
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>
+              <label htmlFor="standard">
+                <input type="radio" name="speed" id="standard" /> Standard
+                Shipping
+              </label>
+            </td>
+            <td style={{ paddingRight: "0", textAlign: "right" }}>FREE</td>
+          </tr>
+          <tr>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>
+              <label htmlFor="express">
+                <input type="radio" name="speed" id="express" /> 2 Day Express
+                Shipping
+              </label>
+            </td>
+            <td style={{ paddingRight: "0", textAlign: "right" }}>₹500</td>
           </tr>
         </tbody>
       </table>
       <div className="line-spacer"></div>
+      <h5 className="clr-accent">Select Payment Method</h5>
+      <table className="table table-borderless">
+        <tbody>
+          <tr>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>
+              <label htmlFor="COD">
+                <input type="radio" name="payment" id="COD" /> Cash on Delivery
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>
+              <label htmlFor="razorpay">
+                <input type="radio" name="payment" id="razorpay" />{" "}
+                <img src={razorpay} alt="" />
+              </label>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <table className="table table-borderless end-point">
         <tbody>
           <tr>
-            <td>Grand Total</td>
-            <td> ₹456454 </td>
+            <td style={{ paddingLeft: "0", textAlign: "left" }}>Grand Total</td>
+            <td style={{ paddingRight: "0", textAlign: "right" }}>
+              116,237.08
+            </td>
           </tr>
           <tr>
             <td colSpan="2">
