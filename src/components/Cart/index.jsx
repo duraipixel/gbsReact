cc6da4757ae3fa5399c086b7fd3221aeb7a99eaa
Fahd <fahd.pixel@gmail.com>
2023-03-28 09:29:04 +0530

    FEAT: Cart Page Folder Structure

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./styles.css";
 import { Col, Container, Row } from "react-bootstrap";
-import ProductDetails from "./ProductDetails";
-import CartDetails from "./CartDetails";
+import ProductDetails from "./ProductDetails/ProductDetails";
+import CartDetails from "./CartDetails/CartDetails";
 
 const CartProduct = () => {
   return (
@@ -14,7 +14,7 @@ const CartProduct = () => {
               <ProductDetails />
             </Col>
 
-            <Col lg={{ span: 3, offset: 1 }} className="card-cart p-4 align-self-start">
+            <Col lg={{ span: 3, offset: 1 }} className="card-cart p-4 align-self-start pb-0">
               <CartDetails />
             </Col>
           </Row>
