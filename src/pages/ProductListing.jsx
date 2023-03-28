d1412c3585f0cbef14f7a36ead7f6614c887a406
behin-pixel <behin.pixel@gmail.com>
2023-03-28 21:38:52 +0530

    New Folder

---
+++ src/pages/ProductListing.jsx
@@ -0,0 +1,15 @@
+import Header from "layouts/Header/Header";
+import Footer from "layouts/Footer/Footer";
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div>
+      <Header />
+       <ProductListView />
+      <Footer />
+    </div>
+  );
+};
+
+export default CartPage;
