f69eb067d5ae4d0e03045af7583db0f9ead0d171
behin-pixel <behin.pixel@gmail.com>
2023-03-30 11:42:55 +0530

    Changes in href

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = ({showToggle}) => {
                   rel="noopener noreferrer"
                   onClick={() => openInNewTab("https://www.instagram.com")}
                 >
-                  <LinkiOutlineInstagram />
+                  <AiOutlineInstagram />
                 </Link>
               </ListGroup.Item>
               <ListGroup.Item>
