80d09c1f04f0ca8a499cb42563881bc02c5456f7
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-10 15:58:04 +0530

    FIX : Header Issue fixing

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,115 +1,61 @@
-import React from "react";
+import "./styles.css";
 import { Link } from "react-router-dom";
-import Logo from "assets/Brand/gbsLogoHighRes.png";
 import close from "assets/images/close.png";
 import { FiUser, FiShoppingCart, FiMenu } from "react-icons/fi";
-import "./styles.css"; 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = ({toggleLeftSideMenu, showToggle, closeToggle}) => {
+const Header = ({ toggleLeftSideMenu, showToggle, closeToggle }) => {
+  const MenuIcon = () => {
+    return (
+      <Link onClick={() => toggleLeftSideMenu()} className="toggle-button">
+        <FiMenu className="font-size-1_5rem" />
+        <div>Menu</div>
+      </Link>
+    )
+  }
+  const CartButton = ({ className,text, size }) => {
+    return (
+      <Link to="/cart" className={`position-relative ${className}`}>
+        <FiShoppingCart  size={size} />
+        {text&&<div>Cart</div>}
+        <span className="cart-count-badge">2</span>
+      </Link>
+    )
+  }
   return (
-    <>
-      <div className="top-bar">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <div className="d-flex align-items-center justify-content-between">
-                <div className="top-logo">
-                  <Link to="/">
-                    <img src={Logo} alt="" />
-                  </Link>
-                </div>
-                <div className="top-search text-center">
-                  <div className="hoger-toggle">
-                    <Link onClick={() => toggleLeftSideMenu()} className="toggle-button">
-                      <FiMenu className="font-size-1_5rem" />
-                      Menu
-                    </Link>
-                  </div>
-                  <div className="form-data">
-                    <input
-                      className="src-blnk"
-                      type="search"
-                      placeholder="Search Your Product ..."
-                    />
-                  </div>
-                </div>
-                <div className="top-icons">
-                  <ul>
-                    <li>
-                      <Link to="">
-                        <FiUser className="font-size-1_5rem" />
-                        <span>My Account</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/cart">
-                        <FiShoppingCart className="font-size-1_5rem" />
-                        <span>Cart</span>
-                      </Link>
-                      <span className="cart-tpimg">2</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Link to="/">
+          <img src={require('assets/Brand/gbsLogoHighRes.png')} alt="logo" />
+        </Link>
+        <div className="d-flex align-items-center">
+          {window.innerWidth < 992 && <CartButton size={30} text={false} className="me-3"/>}
+          <Navbar.Toggle aria-controls="navbarScroll" />
+        </div>
+        <Navbar.Collapse id="navbarScroll" className="py-2">
+          <div className="d-md-flex align-items-center justify-content-between w-100">
+            <div className="d-flex align-items-center text-center search-wrapper">
+              {window.innerWidth > 992 && <MenuIcon />}
+              <Form.Control type="search" className="ms-lg-3" placeholder="Search Your Product ..." />
+            </div>
+            <div className="d-lg-flex align-items-center text-center justify-content-center nav-menu">
+              {window.innerWidth < 992 && <MenuIcon />}
+              <Link to="" className="ps-3">
+                <FiUser className="font-size-1_5rem" />
+                <div>My Account</div>
+              </Link>
+              {window.innerWidth > 992 && <CartButton size={22} text={true} className="ps-lg-4 ps-3" />}
             </div>
           </div>
-        </div> 
-      </div> 
-        
-        <div className={`togle-menu ${showToggle ? 'show' : ''}`} id="toggle_left_menu">
-              <div className="togmenu-header">
-                  <Link to="" className="clse-menu" onClick={() => closeToggle()}>
-                    <img src={close} alt="" /></Link>
-                  <h4>What are you looking<br /> for today?</h4>
-              </div>
-              <div className="togmenu-lists">
-              <div className="navigation">
-                <ul>
-                  {/* <li className="has-sub"> <Link to="#">Menu 1</Link>
-                    <ul>
-                    <li className="has-sub"> <Link to="#">Submenu 1.1</Link>
-                      <ul>
-                      <li><Link to="#">Submenu 1.1.1</Link></li>
-                      <li className="has-sub"><Link to="#">Submenu 1.1.2</Link>
-                        <ul>
-                        <li><Link to="#">Submenu 1.1.2.1</Link></li>
-                        <li><Link to="#">Submenu 1.1.2.2</Link></li>
-                        </ul>
-                      </li>
-                      </ul>
-                    </li>
-                    <li><Link to="#">Submenu 1.2</Link></li>
-                    </ul>
-                  </li> */}
-                  <li className="has-sub"> <Link to="#">Laptops</Link>
-                    <ul>
-                    <li><Link to="#">All Laptops</Link></li>
-                    <li><Link to="#">for Casual & Everyday Use</Link></li>
-                    <li><Link to="#">for Business</Link></li>
-                    <li><Link to="#">for Content Creators</Link></li>
-                    <li><Link to="#">for Gaming</Link></li>
-                    </ul>
-                  </li>
-                  <li className="has-sub"> <Link to="#">Desktops </Link>
-                    <ul>
-                    <li><Link to="#">All Desktops</Link></li>
-                    <li><Link to="#">for Casual & Everyday Use</Link></li>
-                    <li><Link to="#">for Business</Link></li>
-                    <li><Link to="#">for Content Creators</Link></li>
-                    <li><Link to="#">for Gaming</Link></li>
-                  </ul>
-                  </li>
-                  <li><Link to="#"> Tablets </Link></li>
-                  <li><Link to="#"> Top Deals </Link></li>
-                  <li><Link to="#"> Shop by Brand </Link></li>
-                  <li><Link to="#"> GBS Service </Link></li>
-                  <li><Link to="#"> Store Finder </Link></li>
-                </ul>
-              </div>
-              </div>
-        </div>    
-    </>
-  );
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 };
 
 export default Header;
