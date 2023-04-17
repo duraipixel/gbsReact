a4b7632430d8d70a48b93d188836d3c68325bcf9
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-17 16:51:41 +0530

    FEAT : Product Overview View UI

---
+++ src/components/Product/ProductOverview.jsx
@@ -0,0 +1,19 @@
+function ProductOverview() {
+    return (
+        <div>
+            <hr />
+            <h5>Product Overview</h5>
+            <ul className="bullet-points mt-3">
+                <li><span className="text-info">CPU</span>: Intel® Core™ i7-11800H processor Hexa-core 2.30 GHz</li>
+                <li><span className="text-info">GPU</span>: NVIDIA GeForce GTX 1650 GDDR6 4 GB Laptop GPU</li>
+                <li><span className="text-info">RAM</span>: 8GB DDR4 3200MHz</li>
+                <li><span className="text-info">Storage</span>: 512 GB PCIe NVMe SSD</li>
+                <li><span className="text-info">Display</span>: Acer ComfyView 15.6” FHD IPS Display with 144 Hz Refresh Rate</li>
+                <li><span className="text-info">OS</span>: Windows 11 Home 64-bit </li>
+            </ul>
+            <hr />
+        </div>
+    )
+}
+
+export default ProductOverview
