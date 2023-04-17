57bec1e0f5a7f78a8e307d75b41b7df98e8dbac4
Fahd <fahd.pixel@gmail.com>
2023-04-17 17:27:54 +0530

    FEAT: Order Detail Styling

--- src/components/MyAccount/MyOrders/MyOrderDetails/MyOrderDetails.jsx
+++ src/components/MyAccount/MyOrders/MyOrderDetails/MyOrderDetails.jsx
@@ -15,6 +15,38 @@ const MyOrderDetails = () => {
         </div>
         <div>
           <p>Your order has been shipped and it’s out for delivery</p>
+          <div className="p-2"></div>
+        </div>
+        <div>
+          <div className="flex-jc-btwn align-c t-head-title">
+            <p>Product</p>
+            <div className="flex-jc-btwn align-c gap-8rem">
+              <p>Quantity</p>
+              <p>Price</p>
+            </div>
+          </div>
+          <hr className="mt-0" />
+          <div className="flex-jc-btwn pt-1">
+            <div className="flex gap-3 align-c flex-wrap">
+              <img src={require("assets/images/Cart/Cart1.png")} alt="" />
+              <p>ASUS ROG Strix G17 - G713IE-HX040W</p>
+            </div>
+            <div className="flex-jc-btwn gap-8rem align-c">
+              <p>1</p>
+              <p>₹89,306</p>
+            </div>
+          </div>
+          <hr />
+          <div className="flex-jc-btwn pt-1">
+            <div className="flex flex-jc-btwn gap-3 align-c flex-wrap">
+              <img src={require("assets/images/Cart/warranty.png")} alt="" />
+              <p>Extended Warranty - 1 Year</p>
+            </div>
+            <div className="flex-jc-btwn gap-8rem align-c">
+              <p>1</p>
+              <p>₹6000</p>
+            </div>
+          </div>
         </div>
       </Col>
     </>
