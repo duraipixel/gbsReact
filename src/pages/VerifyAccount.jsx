cd1e765a4f5dff9e343e4d9d77611fd1b75af287
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-20 19:13:16 +0530

    FEAT : VerfiyAccountHandler

--- src/pages/VerifyAccount.jsx
+++ src/pages/VerifyAccount.jsx
@@ -1,12 +1,15 @@
 import { useMemo } from "react"
 import { Spinner } from "react-bootstrap"
 import { toast } from "react-hot-toast"
+import { useDispatch } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
+import { setAuthUser } from "redux/features/authSlice"
 import { VerfiyAccountApi } from "services/auth.service"
 
 function VerifyAccount() {
     const { token } = useParams()
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const VerfiyAccountHandler = async () => {
         const response = await VerfiyAccountApi(token)
@@ -14,6 +17,11 @@ function VerifyAccount() {
             toast.error(response.data.message)
         } else {
             toast.success(response.data.message)
+            localStorage.setItem("user_verfied", true)
+            dispatch(setAuthUser({
+                data: response.data.customer,
+                isLoggedIn: true
+            }))
         }
         navigate('/')
     }
