e8d2eff92175992254c4e0594f35fca6208a95ef
Fahd <fahd.pixel@gmail.com>
2023-04-25 11:26:50 +0530

    FEAT: Store And Service Map Implemented

--- src/pages/ServiceCenterDetails.jsx
+++ src/pages/ServiceCenterDetails.jsx
@@ -1,6 +1,7 @@
 import ServiceCenterDetailBanner from "components/ServiceCenter/ServiceCenterDetails/ServiceCenterDetailsBanner";
 import ServiceCenterLocationDetails from "components/ServiceCenter/ServiceCenterDetails/ServiceCenterLocationDetails";
 import ServiceWeOffer from "components/ServiceCenter/ServiceCenterDetails/ServiceWeOffer";
+import ServiceStoreMap from "components/ServiceCenter/ServiceStoreMap/ServiceStoreMap";
 import React, { useEffect } from "react";
 import { scrollToTop } from "utils";
 
@@ -13,6 +14,7 @@ const ServiceCenterDetails = () => {
       <ServiceCenterDetailBanner />
       <ServiceCenterLocationDetails />
       <ServiceWeOffer />
+      <ServiceStoreMap/>
     </>
   );
 };
