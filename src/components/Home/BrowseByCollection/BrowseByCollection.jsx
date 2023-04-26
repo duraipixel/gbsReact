3b1c7cd8b98e5fe8889503d1ae6b78128f0446da
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-26 15:04:46 +0530

    FEAT : Browse By Collection API

---
+++ src/components/Home/BrowseByCollection/BrowseByCollection.jsx
@@ -0,0 +1,39 @@
+import { Image } from "utils";
+import "./styles.css";
+import { useSelector } from "react-redux";
+
+const BrowseByCollection = () => {
+  const browseHome = useSelector(state => state.homePageCollection.browseHome)
+  if (browseHome) return (
+    <>
+      {
+        browseHome.map((browse, index) => (
+          <>
+            <section className="browse-price text-center" key={index}>
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="comon-heads">
+                      <h2>{browse.title}</h2>
+                    </div>
+                  </div>
+                  {
+                    browse.children.map(item => (
+                      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12" key={item}>
+                        <div className="deals-imgs">
+                          <Image src={item.path} alt={item.start_size} className="img-fluid" />
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </section>
+          </>
+        ))
+      }
+    </>
+  );
+};
+
+export default BrowseByCollection;
