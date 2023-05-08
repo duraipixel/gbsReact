8c868aee0b070d05b17edabf5dc96d9921d03918
Fahd <fahd.pixel@gmail.com>
2023-05-08 18:29:17 +0530

    Refactor

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -31,7 +31,7 @@ function AddressForm() {
   const [pincodeMaster, setPincodeMaster] = useState([])
   const getMasters = async () => {
     const { data } = await customerAddressApi()
-    console.log(data);
+    // console.log(data);
     if (data?.status === "success") {
       setAdressMaster(data.address_type)
       setStateMaster(data.state)
