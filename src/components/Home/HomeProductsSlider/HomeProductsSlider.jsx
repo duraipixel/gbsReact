61784197ff2bc28e59addc669537455428d962c2
Fahd <fahd.pixel@gmail.com>
2023-05-09 18:12:35 +0530

    FIX: Banner with Clickable Links

--- src/components/Home/HomeProductsSlider/HomeProductsSlider.jsx
+++ src/components/Home/HomeProductsSlider/HomeProductsSlider.jsx
@@ -1,18 +1,27 @@
 import "./styles.scss";
 import { Placeholder } from "react-bootstrap";
 import BannerSlider from "./BannerSlider";
-import { Image } from "utils";
+import { Image, openInNewTab } from "utils";
 import { useSelector } from "react-redux";
 
 const HomeProductsSlider = () => {
-  const banners = useSelector(state => state.homePageCollection.banners)
-  if (banners) return (
-    <BannerSlider>
-      {banners.map((item) => (
-        <Image key={item.id} alt={item.title} className="w-100" src={window.innerWidth < 768 ? item.mobile_banner : item.image} />
-      ))}
-    </BannerSlider>
-  );
+  const banners = useSelector((state) => state.homePageCollection.banners);
+  console.log(banners);
+  if (banners)
+    return (
+      <BannerSlider>
+        {banners.map((item) => (
+          <div className="cursor" onClick={() => openInNewTab(item.links)}>
+            <Image
+              key={item.id}
+              alt={item.title}
+              className="w-100"
+              src={window.innerWidth < 768 ? item.mobile_banner : item.image}
+            />
+          </div>
+        ))}
+      </BannerSlider>
+    );
   if (!banners)
     return (
       <Placeholder as="p" animation="glow">
