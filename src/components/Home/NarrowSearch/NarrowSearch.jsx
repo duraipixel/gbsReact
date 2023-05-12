2388c9b60c9fd88e0a2ea3d4b6766a2b40049e0f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-05-12 13:13:49 +0530

    FIX : NarrowSearch & Service Center UI

--- src/components/Home/NarrowSearch/NarrowSearch.jsx
+++ src/components/Home/NarrowSearch/NarrowSearch.jsx
@@ -1,38 +1,28 @@
 import "./styles.css";
 import { useSelector } from "react-redux";
 import { Image } from "utils";
-import ProductSlider from "components/ProductSlider/ProductSlider";
 import { Link } from "react-router-dom";
 
 const NarrowSearch = () => {
   const brands = useSelector(state => state.homePageCollection.brands)
   if (brands) return (
     <>
-      <section className="narroww-search text-center">
+      <div className="section-wrapper">
         <div className="container">
-          <div className="row">
-            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div className="comon-heads">
-                <h2>Narrow Your Search to the Brands you Trust</h2>
-              </div>
-            </div>
-
-            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div className="brands-list-slider">
-                <ProductSlider slidesToShow={3}>
-                  {
-                    brands.map((item, i) => (
-                      <Link to={`products?brands=${item.slug}`} key={i} className="brnd-sector">
-                        <Image src={item.image} alt={item.title} className="img-fluid poster-image"/>
-                      </Link>
-                    ))
-                  }
-                </ProductSlider>
-              </div>
-            </div>
+          <h2 className="section-title">Narrow Your Search to the Brands you Trust</h2>
+          <div className="row g-3 justify-content-center">
+            {
+              brands.map((item, i) => (
+                <div className="col-6 col-xl col-lg-3 col-md-4">
+                  <Link to={`products?brands=${item.slug}`} key={i}>
+                    <Image src={item.image} alt={item.title} className="w-100" />
+                  </Link>
+                </div>
+              ))
+            }
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
--- src/components/Home/ServiceCenter/ServiceCenter.jsx
+++ src/components/Home/ServiceCenter/ServiceCenter.jsx
@@ -13,7 +13,6 @@ const ServiceCenter = () => {
                 <h2>Your PC Ran into Trouble?</h2>
               </div>
             </div>
-
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <div className="details-locations">
                 <span>
