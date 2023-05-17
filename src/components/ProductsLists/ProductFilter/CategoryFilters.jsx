4a1e6e861bd3c875f4f0886420752e60550f8a8c
Fahd <fahd.pixel@gmail.com>
2023-05-17 11:49:48 +0530

    FEAT: Product List Category API integration

--- src/components/ProductsLists/ProductFilter/CategoryFilters.jsx
+++ src/components/ProductsLists/ProductFilter/CategoryFilters.jsx
@@ -1,36 +1,46 @@
-import { useEffect, useState } from "react"
-import { Nav } from "react-bootstrap"
-import { useLocation, useNavigate } from "react-router-dom"
-import { subcategoryMenuApi } from "services/filters.service"
+import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
+import { productListCategoryMenuApi } from "services/filters.service";
 
-function CategoryFilters({setCurrentLocation}) {
-    const [subcategory, setSubcategory] = useState([])
-    const location = useLocation();
-    const navigate = useNavigate();
-    const searchParams = new URLSearchParams(location.search);
-    const categoryHandler = (type, value) => {
-        searchParams.set(type, typeof (value) === 'object' ? value.join("_") : value);
-        setCurrentLocation(`?${searchParams.toString()}`)
-        navigate(`/products?${searchParams.toString()}`);
-    }
-    useEffect(() => {
-        subcategoryMenuApi().then((response) => {
-            setSubcategory(response.data)
+function CategoryFilters({ setCurrentLocation }) {
+  const [subcategory, setSubcategory] = useState([]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const searchParams = new URLSearchParams(location.search);
+
+  const categoryHandler = (type, value) => {
+    searchParams.set(type, typeof value === "object" ? value.join("_") : value);
+    setCurrentLocation(`?${searchParams.toString()}`);
+    navigate(`/products?${searchParams.toString()}`);
+  };
+  useEffect(() => {
+    searchParams.toString() === "categories=laptop-laptop"
+      ? productListCategoryMenuApi("laptop").then((response) => {
+          //   console.log(response.data.child_category);
+          setSubcategory(response.data.child_category);
         })
-    }, [])
-    return (
-        <Nav className="justify-content-center bg-dark text-light">
-            {
-                subcategory.length > 0 ?
-                    subcategory.map((item, i) => (
-                        <Nav.Item key={i}>
-                            <Nav.Link onClick={() => categoryHandler('categories',item.slug)} className="text-light">{item.name}</Nav.Link>
-                        </Nav.Item>
-                    ))
-                    : ''
-            }
-        </Nav>
-    )
+      : productListCategoryMenuApi().then((response) => {
+          //   console.log(response.data.child_category);
+          setSubcategory(response.data.child_category);
+        });
+  }, []);
+  return (
+    <Nav className="justify-content-center bg-dark text-light">
+      {subcategory.length > 0
+        ? subcategory.map((item, i) => (
+            <Nav.Item key={i}>
+              <Nav.Link
+                onClick={() => categoryHandler("categories", item.slug)}
+                className="text-light"
+              >
+                {item.name === "LAPTOP" ? "All Laptops" : item.name}
+              </Nav.Link>
+            </Nav.Item>
+          ))
+        : ""}
+    </Nav>
+  );
 }
 
-export default CategoryFilters
+export default CategoryFilters;
--- src/services/filters.service.js
+++ src/services/filters.service.js
@@ -1,14 +1,35 @@
-import axios from 'axios'
-import { AuthUser } from 'utils'
+import axios from "axios";
+import { AuthUser } from "utils";
 
 const filterMenuApi = async (payload) => {
-    return await axios.post(`${process.env.REACT_APP_BASE_URL}/get/filter/sidemenus`, payload)
-}
+  return await axios.post(
+    `${process.env.REACT_APP_BASE_URL}/get/filter/sidemenus`,
+    payload
+  );
+};
 const subcategoryMenuApi = async (payload) => {
-    return await axios.post(`${process.env.REACT_APP_BASE_URL}/get/subcategory/collections`, payload)
-}
+  return await axios.post(
+    `${process.env.REACT_APP_BASE_URL}/get/subcategory/collections`,
+    payload
+  );
+};
+const productListCategoryMenuApi = async (payload) => {
+  return await axios.post(
+    `${process.env.REACT_APP_BASE_URL}/get/product/list/category`,
+    { category_slug: payload }
+  );
+};
 const productsApi = async (search, take) => {
-    const product_search = `${search !== '' ? search : '?'}&customer_id=${AuthUser().id}&take=${take}`
-    return await axios.get(`${process.env.REACT_APP_BASE_URL}/get/products${product_search}`)
-}
-export { filterMenuApi, productsApi, subcategoryMenuApi };
+  const product_search = `${search !== "" ? search : "?"}&customer_id=${
+    AuthUser().id
+  }&take=${take}`;
+  return await axios.get(
+    `${process.env.REACT_APP_BASE_URL}/get/products${product_search}`
+  );
+};
+export {
+  filterMenuApi,
+  productsApi,
+  subcategoryMenuApi,
+  productListCategoryMenuApi,
+};
