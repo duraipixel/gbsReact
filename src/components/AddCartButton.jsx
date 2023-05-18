1fb315ad8b41f041bef05c02ae68d15d3877ffef
Fahd <fahd.pixel@gmail.com>
2023-05-18 15:09:59 +0530

    FIX: UI Issues

--- src/components/AddCartButton.jsx
+++ src/components/AddCartButton.jsx
@@ -1,80 +1,101 @@
-import { useState } from "react"
-import { toast } from "react-hot-toast"
-import { useDispatch } from "react-redux"
-import { removeCart, setCart } from "redux/features/cartSlice"
-import { addToCartApi, removeFromCartApi } from "services/product.service"
-import { AuthUser, LoadingSpinner, checkCartBucket, strRandom } from "utils"
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { removeCart, setCart } from "redux/features/cartSlice";
+import { addToCartApi, removeFromCartApi } from "services/product.service";
+import { AuthUser, LoadingSpinner, checkCartBucket, strRandom } from "utils";
 
 function AddCartButton({ className, product, type, setCartId }) {
-    const dispatch = useDispatch()
-    const [isAddCart, setIsAddCart] = useState(checkCartBucket(product.id))
-    const [loading, setLoading] = useState(false)
-    const addOrRemoveCart = () => {
-        console.log(product.stock_status)
-        setLoading(true)
-        if (checkCartBucket(product.id)) {
-            removeFromCartApi({
-                product_id: product.id,
-                customer_id: AuthUser()?.id,
-                guest_token: localStorage.getItem('guest_token'),
-            }).then((response) => {
-                if (response.data.error === 0) {
-                    toast.success(response.data.message);
-                    dispatch(removeCart(product))
-                    setIsAddCart(false)
-                } else {
-                    toast.error('Network Error')
-                }
-            })
+  const dispatch = useDispatch();
+  const [isAddCart, setIsAddCart] = useState(checkCartBucket(product.id));
+  const [loading, setLoading] = useState(false);
+  const addOrRemoveCart = () => {
+    console.log(product.stock_status);
+    setLoading(true);
+    if (checkCartBucket(product.id)) {
+      removeFromCartApi({
+        product_id: product.id,
+        customer_id: AuthUser()?.id,
+        guest_token: localStorage.getItem("guest_token"),
+      }).then((response) => {
+        if (response.data.error === 0) {
+          toast.success(response.data.message);
+          dispatch(removeCart(product));
+          setIsAddCart(false);
         } else {
-            addToCartApi({
-                product_id: product.id,
-                customer_id: AuthUser()?.id,
-                guest_token: localStorage.getItem('guest_token'),
-                quantity: 1
-            }).then((response) => {
-                if (response.data.error === 0) {
-                    dispatch(setCart(product))
-                    toast.success(response.data.message);
-                    setIsAddCart(true)
-                    if (setCartId !== undefined) {
-                        setCartId(response.data.data.carts[0].cart_id)
-                    }
-                } else {
-                    toast.error('Network Error')
-                }
-            })
+          toast.error("Network Error");
         }
-        setTimeout(() => setLoading(false), 1000)
+      });
+    } else {
+      addToCartApi({
+        product_id: product.id,
+        customer_id: AuthUser()?.id,
+        guest_token: localStorage.getItem("guest_token"),
+        quantity: 1,
+      }).then((response) => {
+        if (response.data.error === 0) {
+          dispatch(setCart(product));
+          toast.success(response.data.message);
+          setIsAddCart(true);
+          if (setCartId !== undefined) {
+            setCartId(response.data.data.carts[0].cart_id);
+          }
+        } else {
+          toast.error("Network Error");
+        }
+      });
     }
-    if (product.stock_status === 'out_of_stock' && type === 'button') {
-        return <button className='btn btn-primary text-white' >out of stock</button>
-    }
-    if (product.stock_status === 'out_of_stock' && type === 'checkbox') {
-        return (
-            <label className='mt-2 text-dark d-block'  >
-                <input type="checkbox" disabled className='form-check-input me-2 rounded-0 border border-dark' />
-                out of stock
-            </label>
-        )
-    }
-    if (type === 'button') return (
-        <button loading={`${loading}`} onClick={addOrRemoveCart} className={isAddCart ? 'btn btn-outline-primary ms-3' : className}>
-            {isAddCart ? "Remove" : "Add to cart"}
-        </button>
-    )
-    if (type === 'checkbox') {
-        const key = strRandom(5);
-        return (
-            <label className='mt-2 text-primary d-block' htmlFor={`Frequently_${key}`} >
-                {
-                    loading ? <LoadingSpinner className="me-2" />
-                        : <input type="checkbox" checked={isAddCart} onChange={addOrRemoveCart} id={`Frequently_${key}`} className='form-check-input me-2 rounded-0 border border-primary' />
-                }
-                Selected
-            </label>
-        )
-    }
+    setTimeout(() => setLoading(false), 1000);
+  };
+  if (product.stock_status === "out_of_stock" && type === "button") {
+    return (
+      <button className="btn btn-primary text-white ms-3">out of stock</button>
+    );
+  }
+  if (product.stock_status === "out_of_stock" && type === "checkbox") {
+    return (
+      <label className="mt-2 text-dark d-block">
+        <input
+          type="checkbox"
+          disabled
+          className="form-check-input me-2 rounded-0 border border-dark"
+        />
+        out of stock
+      </label>
+    );
+  }
+  if (type === "button")
+    return (
+      <button
+        loading={`${loading}`}
+        onClick={addOrRemoveCart}
+        className={isAddCart ? "btn btn-outline-primary ms-3" : className}
+      >
+        {isAddCart ? "Remove" : "Add to cart"}
+      </button>
+    );
+  if (type === "checkbox") {
+    const key = strRandom(5);
+    return (
+      <label
+        className="mt-2 text-primary d-block"
+        htmlFor={`Frequently_${key}`}
+      >
+        {loading ? (
+          <LoadingSpinner className="me-2" />
+        ) : (
+          <input
+            type="checkbox"
+            checked={isAddCart}
+            onChange={addOrRemoveCart}
+            id={`Frequently_${key}`}
+            className="form-check-input me-2 rounded-0 border border-primary"
+          />
+        )}
+        Selected
+      </label>
+    );
+  }
 }
 
-export default AddCartButton
+export default AddCartButton;
