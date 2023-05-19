c0dbe84bfd7c09e0b8096fe1ee10496f71c58e2d
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-05-19 17:09:19 +0530

    FIX : Products Features Tab Active Conditions

--- src/components/Product/ProductFeaturesMenu/ProductsFeaturesTabs.jsx
+++ src/components/Product/ProductFeaturesMenu/ProductsFeaturesTabs.jsx
@@ -2,34 +2,50 @@ import { Tab, Nav } from "react-bootstrap";
 import Description from "./Description";
 import Specification from "./Specification";
 import CustomerReview from "./CustomerReview";
+import { useEffect, useState } from "react";
 
 function ProductsFeaturesTabs({ product }) {
+  const [defaultActiveKey, setDefaultActiveKey] = useState('')
+
+  useEffect(() => {
+    if (product?.description_products?.length !== 0) {
+      setDefaultActiveKey('Description')
+    } else {
+      if (product?.attributes?.length !== 0) {
+        setDefaultActiveKey('ProductSpecifications')
+      } else {
+        if (product.common_review.total > 0) {
+          setDefaultActiveKey('CustomerReview')
+        }
+      }
+    }
+  }, [])
   if (
-    product.description_products.length ||
+    (product.description_products.length ||
     product.attributes.length ||
-    product.common_review.total > 0
+    product.common_review.total > 0) && defaultActiveKey
   )
     return (
       <div className="product-features-tab">
-        <Tab.Container defaultActiveKey="0">
+        <Tab.Container defaultActiveKey={defaultActiveKey.toString()}>
           <Nav variant="pills" className="sticky-top-0 bg-light">
             {product.description_products.length ? (
               <Nav.Item>
-                <Nav.Link eventKey="0">Description</Nav.Link>
+                <Nav.Link eventKey="Description">Description</Nav.Link>
               </Nav.Item>
             ) : (
               ""
             )}
             {product.attributes.length ? (
               <Nav.Item>
-                <Nav.Link eventKey="1">Product Specifications</Nav.Link>
+                <Nav.Link eventKey="ProductSpecifications">Product Specifications</Nav.Link>
               </Nav.Item>
             ) : (
               ""
             )}
             {product.common_review.total > 0 ? (
               <Nav.Item>
-                <Nav.Link eventKey="2">Customer Reviews</Nav.Link>
+                <Nav.Link eventKey="CustomerReview">Customer Reviews</Nav.Link>
               </Nav.Item>
             ) : (
               ""
@@ -37,21 +53,21 @@ function ProductsFeaturesTabs({ product }) {
           </Nav>
           <Tab.Content className="p-3 pt-4">
             {product.description_products.length ? (
-              <Tab.Pane eventKey="0">
+              <Tab.Pane eventKey="Description">
                 <Description product={product.description_products} />
               </Tab.Pane>
             ) : (
               ""
             )}
             {product.attributes.length ? (
-              <Tab.Pane eventKey="1">
+              <Tab.Pane eventKey="ProductSpecifications">
                 <Specification product={product.attributes} />
               </Tab.Pane>
             ) : (
               ""
             )}
             {product.common_review.total > 0 ? (
-              <Tab.Pane eventKey="2">
+              <Tab.Pane eventKey="CustomerReview">
                 <CustomerReview
                   is_review={product.is_review}
                   product={product}
