90cd944285af6b498408e39cb2d14786e402fb55
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-06-02 17:27:31 +0530

    FIX: Address slice update

--- src/redux/features/addressSlice.js
+++ src/redux/features/addressSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const customer_address = JSON.parse(localStorage.getItem("customer_address"));
+const customer_address = JSON.parse(localStorage.getItem("customer_address") !== undefined ?  localStorage.getItem("customer_address") : []);
 const initialState = {
     status: false,
     type: 'CREATE',
