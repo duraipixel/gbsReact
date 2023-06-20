2b7cdd314f71f07cadedf781375eeac352511fe6
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-06-20 17:01:58 +0530

    FIX : About Text Animation Update

--- src/components/AboutUs/AboutBanner.jsx
+++ src/components/AboutUs/AboutBanner.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import banner from "assets/images/banner-1.jpg";
+import ReactTypingEffect from "react-typing-effect";
 
 function AboutBanner() {
   return (
     <div>
-    <section className="p-0">
-        <div className='col-lg-12 col-md-12 col-sm-12'>
-        <img className='w-100' src={banner} alt="banner-pic" />
+      <section className="p-0 mt-lg-5">
+        <div className='col-lg-12 col-md-12 col-sm-12 position-relative'>
+          <img className='w-100' src={banner} alt="banner-pic" />
+          <ReactTypingEffect
+            className="about-text"
+            text={["Excellence", "Resilience", "Determination", "Leadership", "Passion", "Ambitious", "Humility"]}
+            typingDelay={500}
+            eraseDelay={500}
+            cursorRenderer={cursor => <span className="text-dark fw-light">{cursor}</span>}
+          />
         </div>
-    </section>
+      </section>
     </div>
   )
 }
