3355253843cc6338377594f9d461d618f9b31b66
Fahd <fahd.pixel@gmail.com>
2023-07-04 11:17:29 +0530

    ADD: Image Alt text for React Image Gallery

--- src/components/Product/ProductGallery.jsx
+++ src/components/Product/ProductGallery.jsx
@@ -1,7 +1,6 @@
 import ReactImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 export default function ProductGallery({ images, videos }) {
-
   const renderVideo = (item) => {
     return (
       <div className="video-wrapper">
@@ -26,6 +25,8 @@ export default function ProductGallery({ images, videos }) {
       thumbnailLoading: "lazy",
       thumbnailHeight: "76px",
       thumbnailWidth: "76px",
+      originalAlt: "product image",
+      thumbnailAlt: "thumbnail image",
     });
   } else {
     images &&
@@ -38,34 +39,35 @@ export default function ProductGallery({ images, videos }) {
           thumbnailLoading: "lazy",
           thumbnailHeight: "76px",
           thumbnailWidth: "76px",
+          originalAlt: "product image",
+          thumbnailAlt: "thumbnail image",
         })
       );
   }
   if (videos.length) {
-    console.log(videos)
-    videos.map(video => {
-      produtcCollection.push(
-        {
-          embedUrl: video.video_link,
-          thumbnail: video.thumbnail,
-          renderItem: renderVideo.bind(this),
-        } 
-      )
-    })
+    console.log(videos);
+    videos.map((video) => {
+      produtcCollection.push({
+        embedUrl: video.video_link,
+        thumbnail: video.thumbnail,
+        renderItem: renderVideo.bind(this),
+      });
+    });
   }
-  if (produtcCollection.length) return (
-    <div className="mt-2 react-image-gallery-div">
-      <ReactImageGallery
-        slideInterval="4000"
-        thumbnailPosition={window.innerWidth > 769 ? "left" : "bottom"}
-        items={produtcCollection}
-        loading="lazy"
-        lazyLoad={true}
-        showPlayButton={false}
-        showNav={false}
-        showFullscreenButton={false}
-        renderVideo={true}
-      />
-    </div>
-  );
+  if (produtcCollection.length)
+    return (
+      <div className="mt-2 react-image-gallery-div">
+        <ReactImageGallery
+          slideInterval="4000"
+          thumbnailPosition={window.innerWidth > 769 ? "left" : "bottom"}
+          items={produtcCollection}
+          loading="lazy"
+          lazyLoad={true}
+          showPlayButton={false}
+          showNav={false}
+          showFullscreenButton={false}
+          renderVideo={true}
+        />
+      </div>
+    );
 }
