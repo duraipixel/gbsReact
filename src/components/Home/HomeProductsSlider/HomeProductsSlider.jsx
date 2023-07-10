4dc33c07a99fbf09909760b4154acf45b478f9ff
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-10 11:30:24 +0530

    FIX : Banner Slider action link update

--- src/components/Home/HomeProductsSlider/HomeProductsSlider.jsx
+++ src/components/Home/HomeProductsSlider/HomeProductsSlider.jsx
@@ -1,8 +1,9 @@
 import "./styles.scss";
 import { Placeholder } from "react-bootstrap";
 import BannerSlider from "./BannerSlider";
-import { Image, openInNewTab } from "utils";
+import { Image } from "utils";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const HomeProductsSlider = () => {
   const banners = useSelector((state) => state.homePageCollection.banners);
@@ -10,14 +11,14 @@ const HomeProductsSlider = () => {
     return (
       <BannerSlider>
         {banners.map((item) => (
-          <div className="cursor" onClick={() => openInNewTab(item.links)} key={item.id}>
+          <Link className="cursor d-block" to={item.links}  key={item.id}>
             <Image
               alt={item.title}
               className="w-100"
               src={window.innerWidth < 768 ? item.mobile_banner : item.image}
               style={{ minHeight: window.innerWidth < 768 ? 370 : 'auto'}}
             />
-          </div>
+          </Link>
         ))}
       </BannerSlider>
     );
