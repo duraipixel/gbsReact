b73a9095bb94bc392c9815fa0c8ec7d922eb297c
behin-pixel <behin.pixel@gmail.com>
2023-08-10 11:18:26 +0530

    terms and condition

--- src/components/TermsConditions/TermsConditions.jsx
+++ src/components/TermsConditions/TermsConditions.jsx
@@ -20,80 +20,35 @@ function TermsConditions() {
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              <p>
-                Welcome to our <b>GBS SYSTEMS & SERVICES PVT LTD</b> If you
-                continue to browse and use this website you are agreeing to
-                comply with and be bound by the following terms and conditions
-                of use, which together with our privacy policy govern
-                <b>GBS SYSTEMS & SERVICES PVT LTD</b> relationship with you in
-                relation to this website.
-              </p>
-
-              <p>
-                The term GBS SYSTEMS & SERVICES PVT LTD we refers to the owner
-                of the website whose registered office is No.1070-A, Munusamy
-                Salai, KK Nagar West, K. K. Nagar, Chennai, Tamil Nadu 600078.
-                The term ‘you’ refers to the user or viewer of our website.
-              </p>
-
-              <ul>
-                <li>
-                  Please Bring along a copy of our Invoice for Warranty Repairs
-                  if Applicable.
-                </li>
-                <li>
-                  Warranty Clauses stand void for damages caused due to
-                  Mishandling of equipment’s for any reasons causing damage of
-                  their products like lack of knowledge, in proper handling,
-                  electricity problem.
-                </li>
-                <li>
-                  Swollen Battery are Not Covered warranty due to Overcharging
-                  the Inbuilt Battery, User are the Responsible for it.
-                </li>
-                <li>Once invoice made cannot be modified / Cancelled.</li>
-                <li>
-                  GBS SYSTEMS & SERVICES PVT. LTD has all rights to change the
-                  terms and conditions without prior notice.
-                </li>
-              </ul>
-
-              <h2 className="common-heads">Refund & Cancellation Policy</h2>
-
-              <ul>
-                <li>
-                  For Cancellations please contact us via contact us link.
-                </li>
-                <li>
-                  Any request for cancellation to be made within 1hours from the
-                  time of order confirmation.
-                </li>
-                <li>
-                  Once the order is cancelled we will initiate the refund within
-                  the next 24 hours.
-                </li>
-                <li>Once dispatched the order cannot be cancelled.</li>
-              </ul>
-
-              <h3 className="common-heads">What are the available refund options?</h3>
-
-              <p>
-                If <b>GBS SYSTEMS & SERVICES PVT LTD</b> is unable to deliver
-                your order, then complete refund will be made.
-              </p>
-
-              <p>
-                We do not take responsibility, if the delivery is delayed or not
-                delivered by the delivery agent due to bad weather or any other
-                unexpected circumstances beyond our control.
-              </p>
-
-              <p>
-                Refunds will be issued to the customer Bank accounts, please
-                allow 7 to 14 working days for the credit to appear in your
-                account, it is the bank’s policy that delays the refund timing
-                and we have no control over this.
-              </p>
+            <h2 className="common-heads">1. General</h2>
+            <ul>
+              <li>GBS Systems & Services Pvt Ltd, having its registered address at No.1070-A, Munusamy Salai, KK Nagar West, K. K. Nagar, Chennai, Tamil Nadu 600078 (“GBS”, “we”, “us” or “our”) is managing and operating the website www.gbssystems.in (collectively referred to as the “GBS Digital Platform”) through which it inter alia facilitates the sale of Laptops, Desktops, tablets, Monitor, Printers, Accessories and other products(“Product(s)”) to the users of the GBS Digital Platform (“User” or “Users” or “you” or “your”).</li>
+              <li>We aim to provide the best customer experience to our Users beginning from placing an order to initiating a return. While transacting on the GBS Digital Platform, you can expect a hassle-free experience in returning the Product that you have ordered and can reply on us as your preferred shopping destination.</li>
+              <li>This cancellation, replacement, return and refund policy (“Policy”), together with the Terms sets out our procedures and policies in accepting Product cancellation, replacement, return and refund thereof.</li>
+            </ul>
+            <h2 className="common-heads">2. Applicability of Policy</h2>
+            <ul>
+              <li>By agreeing to use the GBS Digital Platform and/or initiating a request for purchase of Product(s) on the GBS Digital Platform, you agree to be bound by the terms contained in this Policy without modification. If you do not agree to the terms contained in this Policy, you are advised not to transact on the GBS Digital Platform.</li>
+              <li>Please note that we may from time to time change the terms of the Policy that govern your cancellation, replacement, return and refund of an order for Products on the GBS Digital Platform. Every time you wish to use the GBS Digital Platform, please check the Policy to ensure you understand the terms and conditions that apply at that time.</li>
+            </ul>
+            <h2 className="common-heads">3. Cancellation of Products</h2>
+            <ul>
+              <li>You can cancel orders for Products partially or fully prior to the shipment is “Out for Delivery”. Orders cannot be cancelled once the package is “Out for Delivery”. You can track your order under ‘My Order’ section of your account on Platform or via link send to your email id.</li>
+              <li>In case you change your mind in relation to a particular order of the Product(s), you may cancel the placed purchase order on Platform under “My Orders” section of your account.</li>
+              <li>On receipt of the cancellation request, we will attempt to cancel the order/shipments as long as it is not “Out for Delivery”. You will receive an email as soon as the cancellation is accepted and the amount paid towards the partial/full order will be refunded to you within 3-7 (three to seven) business days from the date of acceptance of cancellation request. The amount shall be refunded to you through the same mode of payment used by you for the purchase or via credit to your store credit account which can be used for subsequent purchases. All refunds, except for refund to store credit account, shall be subject to our discretion, applicable policies and charges of the User’s bank/ financial institution.</li>
+              <li>We reserve the right to cancel your bulk orders placed on the Platform. To avoid such cancellations and/or for bulk orders, please visit our Contact us section or E-mail to <b>Support@gbssystems.com</b></li>
+            </ul> 
+            <h2 className="common-heads">4. Replacement or Exchange of Products</h2>
+            <ul>
+              <li>Product is received in a physical damaged condition, to be reported to us within 24 hours of delivery or before activation of products like Laptops, Tablets & Desktops, whichever is earlier.</li>
+              <li>Product is defective or is not in working condition.</li>
+              <li>Product or parts of the Product or accessory is missing, to be reported to us within 24 hours of delivery or before activation of the products like Laptops, Tablets, Desktop, whichever is earlier.</li>
+              <li>You shall keep the Products in its unused, original condition, along with the original invoice/ sale receipt, brand outer box, MRP tags attached, user manual, warranty cards, and original accessories in manufacturer packaging for a successful replacement pick-up. We would accept the request for replacement of such Product subject to the terms of this Policy.</li>
+              <li>If you have received any Product which is subject to replacement in terms of this Policy, we suggest you to immediately register a request in this regard on the GBS Digital Platform. We will in our sole discretion verify if: <br/> (a) the defects and damages in the Products or inaccuracy claimed in such Products are acceptable by us, or <br/> (b) such defects, damages or inaccuracy are due to our default, and will accordingly post verification, accept or reject the request for replacement. For such verification, we may request you to send us images of the damaged, defective or inaccurate Product received and/or allow our personnel to schedule a call or visit to assess the damage, defect in the Product. Alternatively, you could also share the Brand DOA certificate for us to process replacement. Further to the assessment of the damage or the concern reported in the replacement request, we may in our sole discretion resolve the issue subject to the assessment report, in a manner as we may deem fit.</li>
+              <li>Your replacement will be processed only when the conditions as may be stipulated by us are fulfilled at the time of replacement of such Products, such as the Product to be replaced being provided to us in original condition along with the price tag intact including original packaging of the Product, the serial number/ bar code of the Product matches our records, if Product(s) bought as a combo then Product(s) sent for replacement to be as a complete combo, the brand outer packaging of the Product and all accessories therein shall be intact, no damage having occurred post-delivery of the Product while in your possession etc.</li>
+              <li>You agree that we will not replace any Product: <br /> (i) if you have placed the order for a wrong Product model, colour or incorrect Product, <br />  (ii) if the Product belongs to non-replacement Product category  <br /> (iii) if you fail to request replacement/register a complaint in relation to a damaged, defective or inaccurate Product within the Replacement Period <br />  (iv) if products like Mobiles, tablets, laptops are found activated during investigation. Any damage to the Product caused by your improper use of the Product, or any modification or change to the Product by you the User or a third party or any depreciation in the value for other reasons will not be deem such Product a damaged, defective or inaccurate Product and will not be considered a quality problem. Any decision by us in this regard shall be final and binding.</li>
+              <li>You should raise a replacement request at <b>+91 96003 7522</b> or <b>Support@gbssystems.com</b> to get a Replacement Authorization code for any of the eligible reasons. Once we receive your replacement request, we may schedule an engineer call or visit to assess the issue reported. Basis the engineer’s technical report a resolution for replacement will be offered. If the replacement request is approved, we will acknowledge your request for return via an email containing Product details, pickup details and Replacement Authorization Code. Please ensure to collect the manifest copy from the logistic partner while handing over the Product. The manifest copy will be your reference to track the return order for replacement in transit and claim for any losses in transit. Once the returned Product has been received by us for replacement, replacement Product will be shipped to your address entered on the original order.</li>
+            </ul> 
             </div>
           </div>
         </div>
