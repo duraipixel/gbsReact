5f81cfa30faf2caaf649cf46d097930177227f37
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-09-08 12:53:04 +0530

    FIX : Product Description HTML Iner Setup

--- src/components/Product/ProductFeaturesMenu/Description.jsx
+++ src/components/Product/ProductFeaturesMenu/Description.jsx
@@ -4,7 +4,7 @@ function Description({ product }) {
         <div>
             <center>
                 <h2 className='fw-bold'>{product[0]?.title}</h2>
-                <p>{product[0]?.description}.</p>
+                <p dangerouslySetInnerHTML={{ __html: product[0]?.description }}></p>
                 <div className="col-sm-8 col">
                     <img src={product[0]?.desc_image} alt={product[0]?.title} className='w-100' />
                 </div>
@@ -16,7 +16,7 @@ function Description({ product }) {
                             <center>
                                 <img src={item.desc_image} alt={item.title} className='img-fluid' />
                                 <h5 className='fw-bold mt-3'>{item.title}</h5>
-                                <p>{item.description}</p>
+                                <p dangerouslySetInnerHTML={{ __html: item.description }}></p>
                             </center>
                         </div>
                     ))
